117a77d1f159164c92c3e8132901a37db86c0ca4
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-01-11 22:04:35 +0200

    Status: in development; Updates: create Main.tdc component for singlUser in src/components/singlUser/Main.tsx; Modified SingleUser.tsx page component to fetch data.

--- src/pages/SingleUser.tsx
+++ src/pages/SingleUser.tsx
@@ -1,12 +1,30 @@
+import { useEffect } from "react";
 import { Box } from "@mui/material";
+import { Params, useParams } from "react-router-dom";
 
+import { useAppDispatch } from "../redux/hooks";
 import Header from "../components/Header";
+import Main from "../components/singlUser/Main";
+import { postsFetch } from "../redux/reducers/posts";
+import { usersFetch } from "../redux/reducers/users";
+import { commentsFetch } from "../redux/reducers/comments";
 
 export default function SingleUser(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const params: Readonly<Params<string>> = useParams();
+  const urlUser: string = `/${params.id}`;
+  const urlPosts: string = `/?userId=${params.id}`;
+
+  useEffect(function () {
+    dispatch(usersFetch(urlUser));
+    dispatch(postsFetch(urlPosts));
+    dispatch(commentsFetch(""));
+  }, []);
+
   return (
     <Box component="div">
       <Header />
-      <div>singleUser</div>
+      <Main />
     </Box>
   );
 }
